ed79af7201858022b34497d3c07339bbd2f6b1bc
shahroozdev <shahroozdeveloper@gmail.com>
2026-02-15 17:42:32 +0500

    some fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -56,7 +56,7 @@ function App() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:4000/match", {
+      const res = await fetch(import.meta.env.VITE_API_BASE_URL+"match", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: userMsg.content }),
